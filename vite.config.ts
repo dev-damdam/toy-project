f151358452eecc7c18ef59288696768ed63f201b
dev-damdam <dev.sodam@gmail.com>
2023-05-26 15:32:36 +0900

    :bento: 공용 css 파일 추가
    _colors, _utils, reset

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/_utils.scss";
+        @import "@/assets/styles/_colors.scss";`,
+      },
+    },
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
